94cb098fe49c938c9b2ba4b5516106b982ddd5f3
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2025-07-08 15:34:09 +0200

    feat:Add Translator class
    Translator class sends extracted text to LLM for translation.

---
+++ app/pipeline/Translator.py
@@ -0,0 +1,77 @@
+import yaml
+from together import Together
+import json
+
+class Translator:
+    def __init__(self):
+        self._together_api_key = None
+        self.__default_api_keys_file_path = "app/config/api_keys.yaml"
+        self._load_api_key(self.__default_api_keys_file_path)
+        self._init_client()
+        self.system_prompt = """
+You are a professional manga translator. Your task is to translate Japanese text into fluent, natural English that preserves the tone, context, and flow of the original manga dialogue. The translation should be adapted for a native English-speaking audience while staying true to the intent and emotion of the original.
+
+You will be provided an array of JSON objects. Each object has:
+- "id": a unique integer identifier
+- "original": the full content of a single manga speech bubble
+
+Your task is to return the same array of objects, but with an added field:
+- "translation": your natural English translation
+
+⚠️ Important:
+- Translate each bubble independently.
+- Do NOT change the order or structure of the array.
+- Return strictly valid JSON without any additional commentary or formatting.
+"""
+        
+
+    def _load_api_key(self, api_keys_file_path):
+        try:
+            with open(api_keys_file_path, 'r') as file:
+                api_keys_content = yaml.safe_load(file)
+            
+            api_keys = api_keys_content['api_keys']
+            self._together_api_key = api_keys['together_api_key']
+        except:
+            print("Error:Failed to load api key")
+    
+
+    def _init_client(self):
+        try:
+            self._client = Together(api_key=self._together_api_key)
+        except:
+            print("Error:Together client initialization failed")
+
+    def _prepare_batch_for_translation(self, text_results):
+        return [
+            {"id": idx, "original": entry["translation"]["original"]}
+            for idx, entry in enumerate(text_results)
+            if entry["translation"]["original"].strip()
+        ]
+
+
+    def _merge_translations(self, text_results, translated_batch):
+        text_results = text_results.copy()
+        id_to_translation = {entry["id"]: entry["translation"] for entry in translated_batch}
+        for idx, entry in enumerate(text_results):
+            if idx in id_to_translation:
+                entry["translation"]["translation"] = id_to_translation[idx]
+        return text_results
+
+
+    
+    def translate_batch(self, text_batch):
+        batch = self._prepare_batch_for_translation(text_batch)
+        user_prompt = json.dumps(batch, ensure_ascii=False, indent=2)
+
+        response = self._client.chat.completions.create(
+            model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
+            messages=[{"role":"system","content":self.system_prompt}
+                    ,{"role":"user","content":user_prompt}])
+
+        raw_response_text = response.choices[0].message.content
+
+        translated_batch = json.loads(raw_response_text)
+
+        return self._merge_translations(text_batch, translated_batch)
+
--- app/pipeline/executor.py
+++ app/pipeline/executor.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import cv2
 from text_detector.text_detector import TextDetector
 from text_extractor import TextExtractor
+from Translator import Translator
 
 def main():
     file_name = "p (1)"
@@ -16,30 +17,22 @@ def main():
         return
     original_image = image_importer.imported_image
 
-    # Display image for test
-    #---
-    plt.figure(figsize=(8, 8))
-    plt.imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
-    plt.title("Original Image")
-    plt.axis("off")
-    plt.show()
-    #---
-
     text_detector = TextDetector()
 
     hierarchy = text_detector.get_detection_hierarchy(original_image)
-
-    # Display hierarchy for test
-    #---
-    import json
-    print(json.dumps(hierarchy.hierarchy, indent=2))
-    #---
 
     text_extractor = TextExtractor()
 
     extracted_text = text_extractor.extract_text(original_image.copy(), hierarchy)
 
-    print(extracted_text)
+    translator = Translator()
+
+    translation_map = translator.translate_batch(extracted_text)
+
+    # Dispaly translation map for test
+    # ---
+    print(translation_map)
+    # ---
 
 
 if __name__ == '__main__':
