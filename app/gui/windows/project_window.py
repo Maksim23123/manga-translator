ed8f2c4ff5e1adceaf4049431a91ed7662227820
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2025-07-13 16:34:12 +0200

    feat:Add menu bar to the project window

--- app/gui/windows/project_window.py
+++ app/gui/windows/project_window.py
@@ -1,7 +1,9 @@
 from PySide6.QtWidgets import (
-    QMainWindow, QTabWidget, QWidget, QVBoxLayout,
+    QWidget, QVBoxLayout, QApplication, 
+    QMainWindow, QMenuBar, QMenu, QTabWidget, QLabel
 )
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction
 import sys
 from ..tabs.sub_main_window import SubMainWindow
 
@@ -12,11 +14,24 @@ class ProjectWindow(QMainWindow):
         self.setWindowTitle("Manga Translator - [Project]")
         self.resize(1000, 700)
 
+        self.init_menu_bar()
+
         self.tabs = QTabWidget()
         self.setCentralWidget(self.tabs)
 
         for i in range(2):  # Create 2 tabs for demonstration
             self.add_tab(i)
+
+    
+    def init_menu_bar(self):
+        self.menu_bar = self.menuBar()
+
+        file_menu = self.menu_bar.addMenu("File")
+        open_project_action = QAction("Open project", self)
+        new_project_action = QAction("New project", self)
+        file_menu.addAction(open_project_action)
+        file_menu.addAction(new_project_action)
+
 
     def add_tab(self, index):
         sub_main_window = SubMainWindow(index)
