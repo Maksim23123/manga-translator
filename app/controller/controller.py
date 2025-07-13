4b4a473f315c3c498b41b28733405459b356ebdf
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2025-07-13 16:07:34 +0200

    feat:Add controller class

---
+++ app/controller/controller.py
@@ -0,0 +1,5 @@
+from ..core.core import Core
+
+class Contorller:
+    def __init__(self, core: Core):
+        self.core = core
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from gui.main_gui import run_gui
+from core.core import Core
 
 def main():
+    core = Core()
     run_gui()
 
 if __name__ == '__main__':
