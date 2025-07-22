23979578d174cc39f8032d90f69d748c918efe1f
Maksim23123 <102969640+Maksim23123@users.noreply.github.com>
2025-07-22 20:33:54 +0200

    feat:Add PyFlow to project

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
+import os
+import sys
+
+project_root = os.path.abspath(os.path.join(__file__, "..", ".."))  # adjust depth
+sys.path.append(os.path.join(project_root, "app"))
+sys.path.append(os.path.join(project_root, "app", "third_party", "PyFlow"))
+
 from gui.gui import GUI
 from core.core import Core
 from controller.controller import Contorller
-import os
+
 
 def main():
     core = Core()
